14ed28afe3beaeeb3c7451b005db4e9cdbb0dd42
Rakesh <rakeshreddy.vemandla@gmail.com>
2024-09-27 12:55:25 +0530

    vercel changes

--- LMS/settings.py
+++ LMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-koj721257jyh=2a6hy4s+%xhruhmn$9kalpo*yw34%f^92bj1u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
